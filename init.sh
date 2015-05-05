cb6ba8144b279f4da85d567e43e2e1caa19696ec
Aurynn Shaw <self@aurynn.com>
2015-05-05 16:19:15 +1200

    adds nginx support

--- init.sh
+++ init.sh
@@ -4,4 +4,10 @@ apt-get install -y language-pack-en
 apt-get install -y python python-pip
 pip install -r /vagrant/proj/requirements.txt
 
+apt-get install -y nginx
 
+cp /vagrant/nginx/hello.conf /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/hello.conf /etc/nginx/sites-enabled
+rm /etc/nginx/sites-enabled/default
+
+service nginx start
