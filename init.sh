6f88e0b71443c8f93bbd5873c0e2338f9384ee28
Aurynn Shaw <self@aurynn.com>
2015-05-05 16:51:06 +1200

    Hash the config file - see if it's changed.

--- init.sh
+++ init.sh
@@ -9,9 +9,19 @@ for pkg in ${PKGS[@]}; do
     fi
 done
 
+# pip install is a no-op if it's already there
 pip install -r /vagrant/proj/requirements.txt
 
-cp /vagrant/nginx/hello.conf /etc/nginx/sites-available
+LEFT=$(md5sum /etc/nginx/sites-available/hello.conf | awk '{ print $1 }')
+RIGHT=$(md5sum /vagrant/nginx/hello.conf | awk '{ print $1 }')
+
+if [ -e /etc/nginx/sites-available/hello.conf ]; then
+    if [ $LEFT != $RIGHT ]; then
+        # vagrant/nginx is our authoritative source
+        cp /vagrant/nginx/hello.conf /etc/nginx/sites-available
+    fi
+fi
+
 ln -s /etc/nginx/sites-available/hello.conf /etc/nginx/sites-enabled
 rm /etc/nginx/sites-enabled/default
 
