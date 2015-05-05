f6236dbfafa5e8c6ed3f1da5c369e327ea07930c
Aurynn Shaw <self@aurynn.com>
2015-05-05 22:06:34 +1200

    Move md5 checks inside test for existence of hello.conf

--- init.sh
+++ init.sh
@@ -8,8 +8,20 @@ for pkg in ${PKGS[@]}; do
     fi
 done
 
-
+# pip install is a no-op if it's already there
 pip install -r /vagrant/proj/requirements.txt
+
+if [ -e /etc/nginx/sites-available/hello.conf ]; then
+    LEFT=$(md5sum /etc/nginx/sites-available/hello.conf | awk '{ print $1 }')
+    RIGHT=$(md5sum /vagrant/nginx/hello.conf | awk '{ print $1 }')
+    if [ $LEFT != $RIGHT ]; then
+        # vagrant/nginx is our authoritative source
+        cp /vagrant/nginx/hello.conf /etc/nginx/sites-available
+    fi
+else
+    echo "copying nginx config"
+    cp /vagrant/nginx/hello.conf /etc/nginx/sites-available
+fi
 
 cp /vagrant/nginx/hello.conf /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/hello.conf /etc/nginx/sites-enabled
