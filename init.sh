75844b1a8b094d872475c29691242346c5b9376f
Aurynn Shaw <self@aurynn.com>
2015-05-07 14:56:59 +1200

    Removes hello.conf from guards step

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
+if [ -e /etc/nginx/sites-available/hello.conf ]; then
+    rm /etc/nginx/sites-available/hello.conf
+    rm /etc/nginx/sites-enabled/hello.conf
+fi
 # apt-get -y update 
 # if ! dpkg -l language-pack-en 2>&1 > /dev/null; then
 #     apt-get install -y language-pack-en 
